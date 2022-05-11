cffccf98bc7b269e2cb039dedc7427441d21bc89
Yuki-KG <yuki@kawagishi.com>
2022-05-11 21:59:13 +0900

    Modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ErlangBApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ErlangBApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Erlang B Formula',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Erlang B Formula Calculator'),
     );
   }
 }
@@ -48,18 +38,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  double _traffic = 0.0;
+  int _lines = 0;
+  double _blocking = 0.0;
+  String _textA = '';
+  String _textL = '';
+  String _esText = '';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,45 +56,61 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        padding: EdgeInsets.all(64),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            TextField(
+              decoration: new InputDecoration(labelText: "Traffic (erlangs)"),
+              keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true),
+              onChanged: (String value) {
+                setState(() {
+                  _textA = value;
+                  _traffic = double.parse(_textA);
+                });
+              }
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            TextField(
+              decoration: new InputDecoration(labelText: "Number of Lines"),
+              keyboardType: TextInputType.number,
+              onChanged: (String value) {
+                setState(() {
+                  _textL = value;
+                  _lines = int.parse(_textL);
+                });
+              }
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _blocking = ES(_traffic, _lines);
+                    _esText = (_blocking == 0.0) ? '' : 'Blocking Rate = $_blocking %';
+                  });
+                },
+                child: Text('Calculate!'),
+            ),
+
+            /*Text('$_servers servers',
+              style: TextStyle(fontSize: 24),
+            ),*/
+            Text('$_esText',
+              style: TextStyle(fontSize: 24),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+double ES(double a, int s) {
+  int i;
+  double es = 1.0;
+  for(i = 1; i <= s; i++) {
+    es = a * es / (i + a*es);
+  }
+  return(100.0 * es);
 }
